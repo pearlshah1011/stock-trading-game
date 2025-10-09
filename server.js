b73076605384e243ea30a8882bf5a74481aa5ff4
Pearl Shah <141494194+pearlshah1011@users.noreply.github.com>
2025-10-09 14:19:14 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -216,7 +216,8 @@ function broadcastGameState() {
     });
 }
 
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
     loadStockDataFromExcel();
 });
